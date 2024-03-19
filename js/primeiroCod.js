fe44632880e492a871642d4141705dd96428ddbd
Arthur <arthur.barros2@estudante.ifms.edu.br>
2024-03-19 09:39:38 -0300

    calculo

---
+++ js/primeiroCod.js
@@ -0,0 +1,17 @@
+function compare(x) {
+    if (x === '') {
+        alert("O campo está vazio!");
+        return false;
+    }
+    if (x < 5) {
+        if (x < 0) {
+            alert(x+" é menor que 5 e negativo");
+        } else {
+            alert(x+" é menor que 5 e positivo");
+        }
+    } else if (x == 5) {
+        alert(x+" igual a 5");
+    } else {
+        alert(x+" maior que 5");
+    } 
+}
