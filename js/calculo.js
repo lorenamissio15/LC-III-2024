7221195a3fd49a768a30685410de9a9dbf1ae6dc
Arthur <arthur.barros2@estudante.ifms.edu.br>
2024-03-19 11:22:57 -0300

    calculo

--- js/calculo.js
+++ js/calculo.js
@@ -1,15 +1,30 @@
-function calculo(x,y){
-    if (x==='' || y===''){
-        return ('Informe números nos dois campos');
+function calculo(x, y) {
+    if (x === '' || y === '') {
+        alert('Informe números nos dois campos');
+        return false;
     }
-    soma=parseInt(x)+parseInt(y);
-    CalculoString = ("A Soma é igual a "+soma+"<br />");
-    subtrcao=x-y;
-    CalculoString += ("A subtração é igual a "+subtrcao+"<br />");
-    divisao=x/y;
-    CalculoString += ("A divisão é igual a "+divisao+"<br />");
-    multiplicacao=x*y;
-    CalculoString += ("A multiplicação é igual a "+multiplicacao);
+
+    if (maior(x,y)){
+        x1 = y; 
+        y = x;
+        x = x1;
+    }
+    soma = parseInt(x) + parseInt(y);
+    CalculoString = ("A Soma é igual a " + soma + "<br />");
+    subtracao = x - y;
+    CalculoString += ("A subtração é igual a " + subtracao + "<br />");
+    divisao = x / y;
+    CalculoString += ("A divisão é igual a " + divisao + "<br />");
+    multiplicacao = x * y;
+    CalculoString += ("A multiplicação é igual a " + multiplicacao);
     return CalculoString
+}
+
+function maior(x, y) {
+    if (x < y) { 
+        return true;
+    } else { 
+         return false; 
+    }
 }
 // calculo(2,3);
